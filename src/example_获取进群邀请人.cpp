c93f24795803824af7bcb8744c5cbd63b59a4a45
MikuPy2001 <794126318@qq.com>
2018-03-03 11:17:49 +0800

    再改我是喵！喵喵喵~

--- src/example_获取进群邀请人.cpp
+++ src/example_获取进群邀请人.cpp
@@ -2,15 +2,16 @@
 #include <CQMsgCode.h>
 
 #include <string>
+
 using namespace std;
 using namespace CQ;
 
 EVE_Request_AddGroup_EX(AddGroup) {
-	if (eve.subType != 1)return;//不是别人请求入群，忽??
-
-	MsgCode mc(eve.msg);//解析附言
-
-	if (!mc.find("at"))return;//查找是否有酷q??:[CQ:at,qq=xxxx]
-
-	string qq = mc.get("qq");//提取qq
+    if (eve.subType != 1)return;//不是别人请求入群，忽略
+	
+    MsgCode mc(eve.msg);//解析附言
+	
+    if (!mc.find("at"))return;//查找是否有酷q码:[CQ:at,qq=xxxx]
+	
+    string qq = mc.get("qq");//提取qq
 }
